d784bc5c5899a47e370bd9700a27a2f29416a4b9
Nicolas Lacroix <nicolacr06@gmail.com>
2022-11-22 09:29:47 +0100

    Fixing some QA for scripts/analysis.py

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,26 +1,32 @@
 import json
-import tomllib
-from typing import Any
 from pathlib import Path
+from typing import Any
 from uuid import uuid1
 
+import tomllib
 from pylint.lint import Run
 
 NOTEBOOK = "notebooks/github/notebook"
 
-def filter_cells_by(data: dict[str, Any], filter_key: str, filter_value: str) -> tuple[dict[str, Any]]:
-    return tuple(n for n in data['cells'] if n[filter_key] == filter_value)
+
+def filter_cells_by(
+    data: dict[str, Any], filter_key: str, filter_value: str
+) -> tuple[dict[str, Any]]:
+    return tuple(n for n in data["cells"] if n[filter_key] == filter_value)
+
 
 def get_code_cells(data: dict[str, Any]) -> tuple[dict[str, Any]]:
-    return filter_cells_by(data, 'cell_type', 'code')
+    return filter_cells_by(data, "cell_type", "code")
+
 
 def get_markdown_cells(data: dict[str, Any]) -> tuple[dict[str, Any]]:
-    return filter_cells_by(data, 'cell_type', 'markdown')
+    return filter_cells_by(data, "cell_type", "markdown")
 
-with open(f"{NOTEBOOK}.ipynb") as f:
+
+with open(f"{NOTEBOOK}.ipynb", encoding="utf-8") as f:
     notebook = json.load(f)
 
-with open(f"{NOTEBOOK}.toml", "rb") as f:
+with open(f"{NOTEBOOK}.toml", mode="rb", encoding="utf-8") as f:
     notebook_metadata = tomllib.load(f)
 
 print(f"========= Analysing {notebook_metadata['title']} =========\n")
@@ -42,30 +48,34 @@ pylint_scores = []
 for code_cell in code_cells:
     tmp_filename = f"tmp/code_cell_{uuid1()}.py.tmp"
     tmp_path = Path(tmp_filename)
-    with open(tmp_filename, 'w') as f:
-        f.writelines(code_cell['source'])
+    with open(tmp_filename, mode="w", encoding='utf-8') as f:
+        f.writelines(code_cell["source"])
         run = Run([tmp_filename], do_exit=False)
         pylint_scores.append(run.linter.stats.global_note)
     tmp_path.unlink()
 
-print("\nExporting results...", end='')
+print("\nExporting results...", end="")
 
 results = {
-    'title': notebook_metadata['title'],
-    'notebook': f'{NOTEBOOK}.ipynb',
-    'metadata': f'{NOTEBOOK}.toml',
-    'metrics': {
-        'nb_code_cells': len(code_cells),
-        'nb_markdown_cells': len(markdown_cells),
-        'dependencies_installation_detected': '!pip ' in code_cells[0]['source'],
-        'code_quality': {
-            'pylint_scores': pylint_scores,
-        }
-    }
+    "title": notebook_metadata["title"],
+    "notebook": f"{NOTEBOOK}.ipynb",
+    "metadata": f"{NOTEBOOK}.toml",
+    "metrics": {
+        "nb_code_cells": len(code_cells),
+        "nb_markdown_cells": len(markdown_cells),
+        "dependencies_installation_detected": "!pip " in code_cells[0]["source"],
+        "code_quality": {
+            "pylint_scores": pylint_scores,
+        },
+    },
 }
 
-with open(f"results/{notebook_metadata['metadata']['author']}_{notebook_metadata['title']}.json", "w") as f:
-    json.dump(results, f,  indent=4)
+with open(
+    f"results/{notebook_metadata['metadata']['author']}_{notebook_metadata['title']}.json",
+    mode="w",
+    encoding="utf-8",
+) as f:
+    json.dump(results, f, indent=4)
 
 print(json.dumps(results, indent=4))
 
