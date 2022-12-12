d98c85cc5a315ef6f446c8f21bc17e50e2742793
Nicolas Lacroix <nicolacr06@gmail.com>
2022-12-12 15:44:23 +0100

    Fixing analysis_plot not handling files missing profile key

--- scripts/analysis_plot.py
+++ scripts/analysis_plot.py
@@ -8,7 +8,11 @@ def plot_profile():
     results = list(glob.glob('results/**/*.json', recursive=True))
     for result in results:
         with open(result) as f:
-            profile = json.load(f)['profile']
+            json_data = json.load(f)
+            if 'profile' not in json_data:
+                print(f"Skipping {result}...")
+                continue
+            profile = json_data['profile']
             plot = [p['cell_type'] for p in profile]
             plt.plot(range(len(plot)), plot)
     plt.legend(results)
