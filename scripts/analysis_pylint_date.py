46ab84c5fb3f2079bc83602025af112b461816c1
dsv-thomas <thomas.de-sousa-vieira@etu.unice.fr>
2022-12-17 23:09:51 +0100

    Update analysis_pylint_date

--- scripts/analysis_pylint_date.py
+++ scripts/analysis_pylint_date.py
@@ -1,37 +1,46 @@
 import glob
 import json
 import tomllib
-import numpy as np
+from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_profile():
-
+def plot_profile(source: str):
     elems = dict()
-    toml_files = list(glob.glob(('../notebooks/*/*/*.toml'), recursive=True))
+    toml_files = list(glob.glob(('../notebooks/' + source + '/*/*.toml'), recursive=True))
     for toml_file in toml_files:
         with open(toml_file, "rb") as f2:
             data = tomllib.load(f2)
-            elems[data['metadata']['date']] = data['title']
-    elems = dict(sorted(elems.items()))
+            if source == 'github':
+                convert_date = datetime.strptime(data['metadata']['date'], '%d/%m/%Y').date()
+            else:
+                convert_date = datetime.strptime(data['metadata']['date'], '%Y-%m-%d').date()
 
+            elems[data['title']] = convert_date
+    print(len(elems))
     json_results = list(glob.glob('../results/*.json', recursive=True))
     x = []
     y = []
     for key, val in elems.items():
-        res = [i for i in json_results if val in i]
+        res = [i for i in json_results if key in i]
         if len(res) > 0:
             with open(res[0]) as f:
                 json_data = json.load(f)
                 if "metrics" in json_data and json_data['metrics']['code_quality']['pylint_score']['score'] > 0:
-                    x.append(key)
+                    x.append(val)
                     y.append(json_data['metrics']['code_quality']['pylint_score']['score'])
-    ########################
-    fig, ax = plt.subplots()
-    ax.set(ylim=(0, 10), yticks=np.arange(1, 10))
-    ax.plot(x, y, linewidth=2.0)
-    ax.set_xlabel('date')
-    ax.set_ylabel('score')
-    plt.xticks(rotation=70)
-    plt.show()
+    print(len(y))
+    return x, y
 
-plot_profile().strip("\\")[1]
+
+github_result = plot_profile('github')
+kaggle_result = plot_profile('kaggle')
+fig, ax = plt.subplots()
+ax.set(ylim=(0, 10), yticks=np.arange(1, 10))
+github_scatter = ax.scatter(github_result[0], github_result[1], linewidth=2.0, c='red')
+kaggle_scatter = ax.scatter(kaggle_result[0], kaggle_result[1], linewidth=2.0, c='blue')
+ax.legend((github_scatter, kaggle_scatter), ("GitHub", "Kaggle"))
+ax.set_xlabel('date')
+ax.set_ylabel('score')
+plt.xticks(rotation=70)
+plt.show()
